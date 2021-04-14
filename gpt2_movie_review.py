e53ec10a6e50835f95a88d2040cd7db880635ce1
Seungmin Lee <madcat@khu.ac.kr>
2021-04-15 00:21:29 +0900

    added sentiment analyze code

---
+++ gpt2_movie_review.py
@@ -0,0 +1,205 @@
+
+import os
+import tensorflow as tf
+from transformers import TFGPT2Model
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+import gluonnlp as nlp
+from gluonnlp.data import SentencepieceTokenizer
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import numpy as np
+import re
+
+
+import wget
+import zipfile
+
+wget.download('https://github.com/NLP-kr/tensorflow-ml-nlp-tf2/releases/download/v1.0/gpt_ckpt.zip')
+
+with zipfile.ZipFile('gpt_ckpt.zip') as z:
+    z.extractall()
+
+
+# 시각화
+
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_'+string], '')
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.legend([string, 'val_'+string])
+    plt.show()
+
+SEED_NUM = 1234
+tf.random.set_seed(SEED_NUM)
+np.random.seed(SEED_NUM)
+
+
+# 데이터 준비하기
+
+TOKENIZER_PATH = './gpt_ckpt/gpt2_kor_tokenizer.spiece'
+
+tokenizer = SentencepieceTokenizer(TOKENIZER_PATH)
+vocab = nlp.vocab.BERTVocab.from_sentencepiece(TOKENIZER_PATH,
+                                               mask_token=None,
+                                               sep_token='<unused0>',
+                                               cls_token=None,
+                                               unknown_token='<unk>',
+                                               padding_token='<pad>',
+                                               bos_token='<s>',
+                                               eos_token='</s>')
+
+
+BATCH_SIZE = 32
+NUM_EPOCHS = 3
+VALID_SPLIT = 0.1
+SENT_MAX_LEN = 39
+
+DATA_IN_PATH = './data_in/KOR'
+DATA_OUT_PATH = "./data_out/KOR"
+
+
+# 데이터 전처리 준비
+
+DATA_TRAIN_PATH = os.path.join(DATA_IN_PATH, "naver_movie", "ratings_train.txt")
+DATA_TEST_PATH = os.path.join(DATA_IN_PATH, "naver_movie", "ratings_test.txt")
+
+train_data = pd.read_csv(DATA_TRAIN_PATH, header = 0, delimiter = '\t', quoting = 3)
+train_data = train_data.dropna()
+train_data.head()
+
+print("Total # dataset: train - {}".format(len(train_data)))
+
+# 텍스트 전처리
+
+def clean_text(sent):
+    sent_clean = re.sub("[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]", "", sent)
+    return sent_clean
+
+
+# train_data = train_data[:50] # for test
+
+train_data_sents = []
+train_data_labels = []
+
+for train_sent, train_label in train_data[['document', 'label']].values:
+    train_tokenized_text = vocab[tokenizer(clean_text(train_sent))]
+
+    tokens = [vocab[vocab.bos_token]]  
+    tokens += pad_sequences([train_tokenized_text], 
+                            SENT_MAX_LEN, 
+                            value=vocab[vocab.padding_token], 
+                            padding='post').tolist()[0] 
+    tokens += [vocab[vocab.eos_token]]
+
+    train_data_sents.append(tokens)
+    train_data_labels.append(train_label)
+
+train_data_sents = np.array(train_data_sents, dtype=np.int64)
+train_data_labels = np.array(train_data_labels, dtype=np.int64)
+
+# 학습 모델 구성
+class TFGPT2Classifier(tf.keras.Model):
+    def __init__(self, dir_path, num_class):
+        super(TFGPT2Classifier, self).__init__()
+        
+        self.gpt2 = TFGPT2Model.from_pretrained(dir_path)
+        self.num_class = num_class
+        
+        self.dropout = tf.keras.layers.Dropout(self.gpt2.config.summary_first_dropout)
+        self.classifier = tf.keras.layers.Dense(self.num_class, 
+                                                kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=self.gpt2.config.initializer_range), 
+                                                name="classifier")
+        
+    def call(self, inputs):
+        outputs = self.gpt2(inputs)
+        pooled_output = outputs[0][:, -1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        return logits
+
+
+BASE_MODEL_PATH = './gpt_ckpt'
+cls_model = TFGPT2Classifier(dir_path=BASE_MODEL_PATH, num_class=2)
+
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=6.25e-5)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+cls_model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+
+
+model_name = "tf2_gpt2_naver_movie"
+
+earlystop_callback = EarlyStopping(monitor='val_accuracy', min_delta=0.0001, patience=2)
+
+checkpoint_path = os.path.join(DATA_OUT_PATH, model_name, 'weights.h5')
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+if os.path.exists(checkpoint_dir):
+    print("{} -- Folder already exists \n".format(checkpoint_dir))
+else:
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    print("{} -- Folder create complete \n".format(checkpoint_dir))
+    
+cp_callback = ModelCheckpoint(
+    checkpoint_path, monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=True)
+
+history = cls_model.fit(train_data_sents, train_data_labels, 
+                        epochs=NUM_EPOCHS, 
+                        batch_size=BATCH_SIZE,
+                        validation_split=VALID_SPLIT, 
+                        callbacks=[earlystop_callback, cp_callback])
+
+
+plot_graphs(history, 'accuracy')
+
+
+plot_graphs(history, 'loss')
+
+
+# 모델 테스트
+
+test_data = pd.read_csv(DATA_TEST_PATH, header=0, delimiter='\t', quoting=3)
+test_data = test_data.dropna()
+test_data.head()
+
+
+# test_data = test_data[:50] # for test
+
+test_data_sents = []
+test_data_labels = []
+
+for test_sent, test_label in test_data[['document','label']].values:
+    test_tokenized_text = vocab[tokenizer(clean_text(test_sent))]
+
+    tokens = [vocab[vocab.bos_token]]  
+    tokens += pad_sequences([test_tokenized_text], 
+                            SENT_MAX_LEN, 
+                            value=vocab[vocab.padding_token], 
+                            padding='post').tolist()[0] 
+    tokens += [vocab[vocab.eos_token]]
+
+    test_data_sents.append(tokens)
+    test_data_labels.append(test_label)
+
+test_data_sents = np.array(test_data_sents, dtype=np.int64)
+test_data_labels = np.array(test_data_labels, dtype=np.int64)
+
+
+print("num sents, labels {}, {}".format(len(test_data_sents), len(test_data_labels)))
+
+cls_model.load_weights(checkpoint_path)
+
+results = cls_model.evaluate(test_data_sents, test_data_labels, batch_size=1024)
+print("test loss, test acc: ", results)
+
+
+
