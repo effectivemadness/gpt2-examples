bbc371cef29f38cb345f27d7f9f1cbe6dc902302
Seungmin Lee <madcat@khu.ac.kr>
2021-04-15 00:21:41 +0900

    added text generation code

---
+++ gpt2_text_gen.py
@@ -0,0 +1,180 @@
+import os
+
+import numpy as np
+import tensorflow as tf
+
+import gluonnlp as nlp
+from gluonnlp.data import SentencepieceTokenizer
+from transformers import TFGPT2LMHeadModel
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from nltk.tokenize import sent_tokenize
+
+
+import wget
+import zipfile
+
+wget.download('https://github.com/NLP-kr/tensorflow-ml-nlp-tf2/releases/download/v1.0/gpt_ckpt.zip')
+
+with zipfile.ZipFile('gpt_ckpt.zip') as z:
+    z.extractall()
+
+
+class GPT2Model(tf.keras.Model):
+    def __init__(self, dir_path):
+        super(GPT2Model, self).__init__()
+        self.gpt2 = TFGPT2LMHeadModel.from_pretrained(dir_path)
+        
+    def call(self, inputs):
+        return self.gpt2(inputs)[0]
+
+
+
+BASE_MODEL_PATH = './gpt_ckpt'
+gpt_model = GPT2Model(BASE_MODEL_PATH)
+
+
+
+BATCH_SIZE = 16
+NUM_EPOCHS = 10
+MAX_LEN = 30
+TOKENIZER_PATH = './gpt_ckpt/gpt2_kor_tokenizer.spiece'
+
+tokenizer = SentencepieceTokenizer(TOKENIZER_PATH)
+vocab = nlp.vocab.BERTVocab.from_sentencepiece(TOKENIZER_PATH,
+                                               mask_token=None,
+                                               sep_token=None,
+                                               cls_token=None,
+                                               unknown_token='<unk>',
+                                               padding_token='<pad>',
+                                               bos_token='<s>',
+                                               eos_token='</s>')
+
+
+
+def tf_top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-99999):
+    _logits = logits.numpy()
+    top_k = min(top_k, logits.shape[-1])  
+    if top_k > 0:
+        indices_to_remove = logits < tf.math.top_k(logits, top_k)[0][..., -1, None]
+        _logits[indices_to_remove] = filter_value
+
+    if top_p > 0.0:
+        sorted_logits = tf.sort(logits, direction='DESCENDING')
+        sorted_indices = tf.argsort(logits, direction='DESCENDING')
+        cumulative_probs = tf.math.cumsum(tf.nn.softmax(sorted_logits, axis=-1), axis=-1)
+
+        sorted_indices_to_remove = cumulative_probs > top_p
+        sorted_indices_to_remove = tf.concat([[False], sorted_indices_to_remove[..., :-1]], axis=0)
+        indices_to_remove = sorted_indices[sorted_indices_to_remove].numpy().tolist()
+        
+        _logits[indices_to_remove] = filter_value
+    return tf.constant([_logits])
+
+
+def generate_sent(seed_word, model, max_step=100, greedy=False, top_k=0, top_p=0.):
+    sent = seed_word
+    toked = tokenizer(sent)
+    
+    for _ in range(max_step):
+        input_ids = tf.constant([vocab[vocab.bos_token],]  + vocab[toked])[None, :] 
+        outputs = model(input_ids)[:, -1, :]
+        if greedy:
+            gen = vocab.to_tokens(tf.argmax(outputs, axis=-1).numpy().tolist()[0])
+        else:
+            output_logit = tf_top_k_top_p_filtering(outputs[0], top_k=top_k, top_p=top_p)
+            gen = vocab.to_tokens(tf.random.categorical(output_logit, 1).numpy().tolist()[0])[0]
+        if gen == '</s>':
+            break
+        sent += gen.replace('▁', ' ')
+        toked = tokenizer(sent)
+
+    return sent
+
+
+
+generate_sent('이때', gpt_model, greedy=True)
+
+
+generate_sent('이때', gpt_model, top_k=0, top_p=0.95)
+
+
+
+DATA_IN_PATH = './data_in/KOR/'
+TRAIN_DATA_FILE = 'finetune_data.txt'
+
+sents = [s[:-1] for s in open(DATA_IN_PATH + TRAIN_DATA_FILE, encoding='utf-8').readlines()]
+
+
+
+input_data = []
+output_data = []
+
+for s in sents:
+    tokens = [vocab[vocab.bos_token],]  + vocab[tokenizer(s)] + [vocab[vocab.eos_token],]
+    input_data.append(tokens[:-1])
+    output_data.append(tokens[1:])
+
+input_data = pad_sequences(input_data, MAX_LEN, value=vocab[vocab.padding_token])
+output_data = pad_sequences(output_data, MAX_LEN, value=vocab[vocab.padding_token])
+
+input_data = np.array(input_data, dtype=np.int64)
+output_data = np.array(output_data, dtype=np.int64)
+
+
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='accuracy')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, vocab[vocab.padding_token]))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_mean(loss_)
+
+def accuracy_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, vocab[vocab.padding_token]))
+    mask = tf.expand_dims(tf.cast(mask, dtype=pred.dtype), axis=-1)
+    pred *= mask    
+    acc = train_accuracy(real, pred)
+
+    return tf.reduce_mean(acc)
+
+
+
+gpt_model.compile(loss=loss_function,
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=[accuracy_function])
+
+
+
+history = gpt_model.fit(input_data, output_data, 
+                    batch_size=BATCH_SIZE, epochs=NUM_EPOCHS,
+                    validation_split=0.1)
+
+
+
+DATA_OUT_PATH = './data_out'
+model_name = "tf2_gpt2_finetuned_model"
+
+save_path = os.path.join(DATA_OUT_PATH, model_name)
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+gpt_model.gpt2.save_pretrained(save_path)
+
+loaded_gpt_model = GPT2Model(save_path)
+
+
+generate_sent('이때', gpt_model, greedy=True)
+
+
+generate_sent('이때', gpt_model, top_k=0, top_p=0.95)
+
